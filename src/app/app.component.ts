55fef714540c2d6ddb081022622564d5a830320f
Rashid Ahmad <AhmadRa@india.rapidigm.com>
2019-08-06 14:23:41 +0530

    Signed-off-by: Rashid Ahmad <AhmadRa@india.rapidigm.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RashidAhmad';
+  mobile = false;
+  isMobile = false;
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 992;
+    console.log(w);
+    if (w < breakpoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ngOnInit() {
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
+  }
+  
 }
